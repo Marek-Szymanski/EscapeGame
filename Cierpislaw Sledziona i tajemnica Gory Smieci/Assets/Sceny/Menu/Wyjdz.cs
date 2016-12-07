7966da9c28bedda08e846de0d8f22a3002618fe9
Marek-Szymanski <Marek@MacBook-Pro-Marek.local>
2016-12-07 12:34:36 +0100

    Gra

---
+++ Cierpislaw Sledziona i tajemnica Gory Smieci/Assets/Sceny/Menu/Wyjdz.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wyjdz : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void wyjdz()
+	{
+		Application.Quit ();
+	}
+}
---
+++ Cierpislaw Sledziona i tajemnica Gory Smieci/Assets/Sceny/Menu/zmienScene.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class zmienScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		//Button b = gameObject.GetComponent<infoPrzycisk> ();
+		//b.onClick.AddListener (delegate () 
+		//{
+				Debug.Log("123");
+		//});
+
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	public void nowaScena(string nazwa)
+	{
+		Application.LoadLevel (nazwa);
+	}
+
+}
