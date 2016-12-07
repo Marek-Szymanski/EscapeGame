9f16d6f4a1635c80aad19c2fbd4e70935f44b030
Marek-Szymanski <Marek@MacBook-Pro-Marek.local>
2016-12-07 21:05:42 +0100

    Zmiany z 07.12.2016

---
+++ Cierpislaw Sledziona i tajemnica Gory Smieci/Assets/Sceny/Poziom 1/Telefon_skrypt.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Telefon_skrypt : MonoBehaviour {
+	public GameObject zagadkaTelefon;
+	// Use this for initialization
+	void Start () {
+		zagadkaTelefon.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton (0)) {
+			Vector3 pos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			RaycastHit2D hitInfo = Physics2D.Raycast (pos, Vector2.zero);
+			if (hitInfo != null && hitInfo.collider != null) {
+				zagadkaTelefon.SetActive (true);
+
+			} else {
+				Debug.Log ("NIE");
+			}
+		}
+	}
+
+
+}
---
+++ Cierpislaw Sledziona i tajemnica Gory Smieci/Assets/Sceny/Poziom 1/ZamknijTelefon.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZamknijTelefon : MonoBehaviour {
+	public GameObject zagadka;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void onClick()
+	{
+		zagadka.SetActive (false);
+	}
+
+
+}
