12d5bff4e16075a992d1c7aca7fc735f1e97c304
karola1519 <karolina.b.gorska@gmail.com>
2017-01-04 17:27:32 +0100

    ruch cierpislawa poziom 2

---
+++ Cierpislaw Sledziona i tajemnica Gory Smieci/Assets/Sceny/Poziom 2/MoveScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject cierpislawSledziona;
+    public Camera kamera;
+	void Start () {
+
+    }
+
+    // Update is called once per frame
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "YourWallName")  // or if(gameObject.CompareTag("YourWallTag"))
+        {
+            rigidbody.velocity = Vector3.zero;
+        }
+    }
+
+    void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Vector3 temp = new Vector3(0.0f, 0.5f, 0.0f);
+            cierpislawSledziona.transform.position += temp;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Vector3 temp = new Vector3(0.0f, 0.5f, 0.0f);
+            cierpislawSledziona.transform.position -= temp;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector3 temp = new Vector3(0.5f, 0.0f, 0.0f);
+            cierpislawSledziona.transform.position -= temp;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Vector3 temp = new Vector3(0.5f, 0.0f, 0.0f);
+            cierpislawSledziona.transform.position += temp;
+        }
+
+        kamera.transform.position = cierpislawSledziona.transform.position;
+
+        kamera.transform.position -= new Vector3(0.0f,0.0f,10.0f);
+
+
+    }
+}
